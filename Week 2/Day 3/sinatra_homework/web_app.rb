9bcf2a7a55285ce053b366ffaf564ffe46112001
Will C. Fisher, Jr <willcfisherjr@gmail.com>
2014-07-30 23:21:08 -0400

    First Sinatra server-> Fake/Real Resumes

---
+++ Week 2/Day 3/sinatra_homework/web_app.rb
@@ -0,0 +1,27 @@
+# require 'rubygems'
+require 'sinatra'
+require 'Faker'
+
+
+
+get '/' do
+  @name = Faker::Name.name
+  erb :index
+end
+
+
+get '/resume' do
+  erb :resume
+end
+
+get '/faker_test' do
+  @wisdom = Faker::Hacker.say_something_smart
+  erb :faker_test
+end
+
+get '/fake_resume' do
+  @name = Faker::Name.name
+  @email = Faker::Internet.free_email
+  @phone = Faker::PhoneNumber.cell_phone
+  erb :fake_resume, layout: :fake_layout
+end
