334404197e0c611f041a79e1c112e8b2b36fcde1
Will C. Fisher, Jr <willcfisherjr@gmail.com>
2014-08-05 23:09:02 -0400

    actually done. robustly validating. boooooyah, biiiitch

--- Week 3/Day 2/hospital_homework/app/models/patient.rb
+++ Week 3/Day 2/hospital_homework/app/models/patient.rb
@@ -1,12 +1,13 @@
 class Patient < ActiveRecord::Base
-  validate :birth_cannot_be_in_the_future
+  
 
   def birth_cannot_be_in_the_future
-    unless (birth <= Date.today) && birth.instance_of?(Date)
-      errors.add(:birth, "DoB can't be in the future!") 
+    unless birth.instance_of?(Date) && (birth <= Date.today)
+      errors.add(:birth, "must be a DATE before today") 
     end
   end
-
+  validates :birth, presence: true
+  validate :birth_cannot_be_in_the_future
   validates :name, presence: true
   validates :gender, presence: true
   # validates :birth, :timeliness => {:on_or_before => lambda { Date.current }, :type => :date}
