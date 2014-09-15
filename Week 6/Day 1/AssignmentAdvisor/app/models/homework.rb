4c4537274093be0d0028300f54eca5b657627644
Will C. Fisher, Jr <willcfisherjr@gmail.com>
2014-09-15 11:25:35 -0400

    workflow tweaking

--- Week 6/Day 1/AssignmentAdvisor/app/models/homework.rb
+++ Week 6/Day 1/AssignmentAdvisor/app/models/homework.rb
@@ -7,4 +7,21 @@ class Homework < ActiveRecord::Base
   validates :assignment_id, presence: true
   validates :user_id, presence: true
   validates_uniqueness_of :user, scope: [:user_id, :assignment_id]
+  include workflow
+  workflow do
+    state :new do
+      event :review, transitions_to: :reviewing
+    end
+    state :reviewing do
+      event :pass, transitions_to: :complete
+      event :fail, transitions_to: :incomplete
+    end
+    state :incomplete do
+      event :review, transitions_to: :reviewing
+      event :pass, transitions_to: :complete
+    end
+    state :complete do
+    end
+    
+  end
 end
--- Week 6/Day 1/AssignmentAdvisor/db/schema.rb
+++ Week 6/Day 1/AssignmentAdvisor/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140909003842) do
+ActiveRecord::Schema.define(version: 20140915131101) do
 
   create_table "assignments", force: true do |t|
     t.string   "name"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20140909003842) do
     t.string   "notes"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "workflow_state"
   end
 
   create_table "links", force: true do |t|
